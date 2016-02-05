ca0924775d2e0b2c86e6a93212e64a585800a629
Phill S <pmszyman@oakland.edu>
2016-02-05 14:12:38 -0500

    Update getClasses.php

--- Webpages/getClasses.php
+++ Webpages/getClasses.php
@@ -1,20 +1,31 @@
 <?php
+    //include('dbconnect.php');
+ //header("Content-Type: text/javascript; charset=utf-8");
 
-/**
- * @author 
- * @copyright 2016
- */
-      $conn = new mysqli("localhost", "hmbock", "team@480", "hmbock");
-                                  
-      $classArray = array();
+    /**
+    * @author
+    * @copyright 2016
+    */
+    $conn = new mysqli("localhost", "hmbock", "team@480", "hmbock");
 
-      $classSql = "SELECT Class, Class_ID FROM Class WHERE Department_ID=" . $_POST['sel_dep'];
-      $classResult = $conn->query($classSql);
-      while ($row = $classResult->fetch_array()) {
-         echo '<option value ="' . $row['Class_ID'] . '">' . $row['CRN'] . ': ' . $row['Title'] . '</option>';
-      }
-      
-      $conn->close();
+    $classArray = array();
 
+    $deptid = 0;
 
+    //if (isset($_POST['depID'])) {
+        $deptid = $_GET['ID'];
+    //}
+
+    $sql = "SELECT Title, CRN, Class_ID FROM Class WHERE Department_ID=" . $deptid;
+    $classResult = $conn->query($sql) ;
+
+    $data = array();
+    while ($row = $classResult->fetch_array()) {
+        $data[] = array(
+            'id' => $row['Class_ID'],
+            'crn' => $row['CRN'],
+            'title' => $row['Title']
+        );
+    }
+    echo json_encode($data);
 ?>
