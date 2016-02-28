61844ad2fafdc84b49141f81f5d85c06e7961c03
Pranusha Daram <prdaram@oakland.edu>
2016-02-28 17:34:28 -0500

    Edited how events get displayed

--- Webpages/Calendar/events.php
+++ Webpages/Calendar/events.php
@@ -5,14 +5,16 @@ $deets = preg_replace('#[^0-9/]#i', '', $deets);
 include ("connect.php");
 
 $events = '';
-$query = mysql_query('SELECT description FROM events WHERE evdate = "'.$deets.'"');
+$query = mysql_query('SELECT app_title, app_time, description FROM events WHERE evdate = "'.$deets.'"');
 $num_rows = mysql_num_rows($query);
 if($num_rows > 0) {
-	$events .= '<div id="eventsControl"><button onMouseDown="overlay()">Close</button><br /><br /><b> ' . $deets . '</b<br /><br /></div>'; 
+	$events .= '<div id="eventsControl"><button onMouseDown="overlay()">Close</button><br /><br /><b> ' . $deets . '</b><br /><br /></div>'; 
 
 	while($row = mysql_fetch_array($query)){
+		$title = $row['app_title'];
+		$time = $row['app_time'];
 		$desc = $row['description'];
-		$events .= '<div id="eventsBody">' . $desc .'<br /><hr><br /></div>';
+		$events .= '<div id="eventsBody"><b>Title: </b> ' . $title . ' <br/><br/><b>Time: </b> ' . $time . ' <br/><br/><b>Description: </b>' . $desc .'<br /><hr><br /></div>'; 
 	}
 }
 echo $events;
