4ccb103f8d083d5c42b25698c49ff74d444c2a38
Pranusha Daram <prdaram@oakland.edu>
2016-03-19 01:16:17 -0400

    changed to show the Student calendar

--- Webpages/newCal.php
+++ Webpages/newCal.php
@@ -39,7 +39,7 @@
 				</nav>	
                 
                 <div id="content">
-					<iframe src="show_calendar.php" width="1000" height="650" style="border:none"></iframe>
+					<iframe src="stu_show_calendar.php" width="1000" height="650" style="border:none"></iframe>
 				</div>
 			</main>
 			<footer role="contentinfo">
