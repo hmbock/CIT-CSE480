72850a94a512e2a4446a64e1bdf8f0f3fae517e1
Phill S <phillszymanski@gmail.com>
2016-04-08 00:06:29 -0400

    Update cancelApp.php

--- Webpages/cancelApp.php
+++ Webpages/cancelApp.php
@@ -6,23 +6,71 @@
 * @author
 * @copyright 2016
 */
-$conn = new mysqli("localhost", "hmbock", "team@480", "hmbock");
+include ("connect.php");
 
-$aId = 0;
+$aId = $_POST['ID'];
 
-if (isset($_POST['ID'])) {
-    $aId = $_POST['ID'];
+$emailSql = "SELECT events.stu_id, events.staff_id, evdate, app_time, Student.F_Name, Student.L_Name, Student.stu_email, Staff.F_Name, Staff.L_Name, Staff.staff_email FROM events
+              INNER JOIN Student ON events.stu_id = Student.stu_id
+              INNER JOIN Staff ON events.staff_id = Staff.staff_id
+              WHERE id = '$aId'"; 
+$emailResult = mysql_query($emailSql);
+
+while ($row = mysql_fetch_array($emailResult)) {
+  $stuID = $row["stu_id"];
+  $staffID = $row["staff_id"];
+  $date = $row["evdate"];
+  $time = $row["app_time"];
+  $stuFName = $row["Student.F_Name"];
+  $stuLName = $row["Student.L_Name"];
+  $stuEmail = $row["Student.stu_email"];
+  $staffFName = $row["Staff.F_Name"];
+  $staffLName = $row["Staff.L_Name"];
+  $staffEmail = $row["Staff.staff_email"];
 }
 
-$sql = "DELETE FROM Appointments WHERE Appointment_ID =" . $aId;
-$classResult = $conn->query($sql) ;
+//$sql = "DELETE FROM events WHERE id=" . $aId;
+$delSql = 'DELETE FROM events WHERE id=' . $aId;
+$delResult = mysql_query($sql);
 
-if($classResult) {
-    echo "Appointment succesfully cancelled";
+                    $subject = 'Betwixt Booking | Appointment Cancelled'; // Give the email a subject 
+                    $message = "                   
+                    
+                    Your appointment with $staffFName $staffLName at $time on $date has been cancelled. If you would like to reschedule this appointment, please click the link below to login:
+                    
+                    http://www.secs.oakland.edu/~hmbock/login.php
+                    
+                    "; // Our message above including the link. *** CHANGE THE URL ***
+                                         
+                    $headers = 'From:noreply@secs.oakland.edu' . "\r\n"; // Set from headers //*** CHANGE THE URL ***
+                    mail($stuEmail, $subject, $message, $headers);
+                    
+                    
+                    
+                    
+                    $subject = 'Betwixt Booking | Appointment Cancelled'; // Give the email a subject 
+                    $message = "                   
+                    
+                    Your appointment with $stuFName $stuLName at $time on $date has been cancelled. If you would like to login and view your appointments, please click the link below to login:
+                    
+                    http://www.secs.oakland.edu/~hmbock/login.php
+                    
+                    "; // Our message above including the link. *** CHANGE THE URL ***
+                                         
+                    $headers = 'From:noreply@secs.oakland.edu' . "\r\n"; // Set from headers //*** CHANGE THE URL ***
+                    mail($staffEmail, $subject, $message, $headers);
+
+$data;
+
+if($delResult) {
+    $data = "success";
 } else {
-    echo "Something went wrong :(";
+    $data = "nope";
 }
+
+echo $data;
 
 ?>
+
 
 
