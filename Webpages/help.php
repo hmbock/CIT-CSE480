19a5cd974c31ddc3e97ea4003d848b73bda09558
Latrice Walker <lkwalker@oakland.edu>
2016-04-09 17:24:31 -0400

    css conversion

--- Webpages/help.php
+++ Webpages/help.php
@@ -1,15 +1,22 @@
 <html>
  <head>
-	<title>Betwixt Booking Help</title> 
+	<title>Betwixt Booking | Help</title> 
 	<meta charset="utf-8">
-		<meta name= "viewport" content= "width= device-width, initial-scale=1.0">
-		<link href="betwixt.css" rel="stylesheet">
-		<link rel="stylesheet" href="C:\Users\lkwal_000\Google Drive\CIT_CSE 480\Production\css\font-awesome-4.5.0\css\font-awesome.min.css">
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
-		<link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	 <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.5 -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+		
 
 <script type="text/javascript">
 
@@ -23,26 +30,15 @@ $(document).ready(function(){
 	
  </head>
 
- <body>
+<body class="hold-transition login-page">
+	<div class="login-box">
+	  <div class="login-logo">
+		<a href="index.html"><b>Betwixt</b>Booking</a>
+	  </div>
+		<!-- /.login-logo -->
+		<div class="login-box-body">
+		<p class="login-box-msg">MISSION STATEMENT</p>
 	
-	<div id="wrapper">
-		<header>
-			<div class="title">
-				<h1> Betwixt Booking Help </h1>
-			<div>
-		</header>
-		<main role ="main">
-					<nav>
-						<ul>
-						  <li><a href="http://secs.oakland.edu/~hmbock/480Index.php">&nbsp;<i class="fa fa-home"></i>&nbsp;Home</a></li>
-						  <li><a href="http://secs.oakland.edu/~hmbock/about.html">&nbsp;<i class="fa fa-book"></i>&nbsp;About</a></li>
-						  <li><a href="http://secs.oakland.edu/~hmbock/signup.php">&nbsp;<i class="fa fa-user-plus"></i>&nbsp;Sign Up</a></li>
-						  <li><a href="http://secs.oakland.edu/~hmbock/login.php">&nbsp;<i class="fa fa-sign-in"></i> &nbsp; Login</a></li>
-		
-						</ul>
-					</nav>	
-					<div id = "content">
-						<h2>Betwixt Booking Help</h2>
 						<h3>My FAQ</h2>
 						<ul>
 							<li>
@@ -58,15 +54,36 @@ $(document).ready(function(){
 						<div id="answer" class="faqAnswer">To cancel an appointment, click on the appointment you would like to cancel and select the "x" in the corner and you will be provided with confirmation that the appointment has been cancelled.</div></li>
 						</ul>
 
-					</div>
-			</main>
-			<footer role="contentinfo">
-					Copyright &copy; 2016 Betwixt Booking<br>
-					<a href="mailto:betwixtbooking@gmail.com">
-						<address>BetwixtBooking@gmail.com</address>
-					</a>
-			</footer>
-		</div>
+		
+		<td>
+							<a href ="index.html"><button type="button" class="btn btn-block btn-warning btn-lg">Back</button></a>
+						</td>
+		 </div> 
+		 <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 2.2.0 -->
+<script src="plugins/jQuery/jQuery-2.2.0.min.js"></script>
+<!-- Bootstrap 3.3.5 -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+						
+						
+							
+						 
+	
+
  
  
  </body>
