1f01bd935bff3a35947083f658c865b738ac85ff
Phill S <phillszymanski@gmail.com>
2016-04-10 16:47:17 -0400

    Update stu_events.php

--- Webpages/stu_events.php
+++ Webpages/stu_events.php
@@ -9,7 +9,7 @@ $username = $_SESSION['username'];
 include ("connect.php");
 
 $events = '';
-$query = mysql_query('SELECT app_title, app_time, description, stu_id FROM events WHERE evdate = "'.$deets.'" AND stu_id = "'.$id.'"');
+$query = mysql_query('SELECT id, app_title, app_time, description, stu_id FROM events WHERE evdate = "'.$deets.'" AND stu_id = "'.$id.'"');
 $num_rows = mysql_num_rows($query);
 if($num_rows > 0) {
 	$events .= '<div id="eventsControl"><button onMouseDown="overlay()">Close</button><br /><br /><b> ' . $deets . '</b><br /><br /><b> ' . $username . '</b><br /><br /></div>'; 
@@ -18,8 +18,10 @@ if($num_rows > 0) {
 		$title = $row['app_title'];
 		$time = $row['app_time'];
 		$desc = $row['description'];
-		$events .= '<div id="eventsBody"><b>Title: </b> ' . $title . ' <br/><br/><b>Time: </b> ' . $time . ' <br/><br/><b>Description: </b>' . $desc .'<br /><hr><br /></div>'; 
+    $appId = $row['id'];
+	$events .= '<div id="eventsBody"><b>Title: </b> ' . $title . ' <br/><br/><b>Time: </b> ' . $time . ' <br/><br/><b>Description: </b>' . $desc .'<br /><br /><button id="' . $appId . '" onMouseDown="cancel(' . $appId . ')">Cancel Appointment</button><hr><br /></div>'; 
 	}
 }
 echo $events;
 ?>
+
