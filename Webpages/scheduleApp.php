29c9467ecc35513c6c5b04f459ef4b7cf02f599a
Phill S <phillszymanski@gmail.com>
2016-04-11 22:59:11 -0400

    Update scheduleApp.php

--- Webpages/scheduleApp.php
+++ Webpages/scheduleApp.php
@@ -151,19 +151,6 @@ session_start();
         </div>
       </div>
 	  
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
-	  
-      <!-- /.search form -->
-	  
 	  
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
@@ -856,6 +843,7 @@ Title: <input name="title" id="title" type="text" />
                         var date = $('#txtDate').val();
                         var description = $('#description').val();
                         var timeSlot = $('input[type="radio"]:checked').val();
+                        
                         $.ajax({
                             url: 'submitApp.php',
                             data: { staffID : staffID, stuID : stuID, title: title, date:date, description:description, timeSlot : timeSlot},
