b543cf25743428b0817be877fb56d35cee1e8434
Phill S <phillszymanski@gmail.com>
2016-04-11 22:44:27 -0400

    Update staffContacts.php

--- Webpages/staffContacts.php
+++ Webpages/staffContacts.php
@@ -161,7 +161,7 @@ session_start();
       </div>
 	  
         <div class="input-group">
-          <input type="text" name="searchid" class="form-control" id="searchid" placeholder="Search for people by last name" size="35"/>
+          <input type="text" name="searchid" class="form-control" id="searchid" placeholder="Search for people by last name" size="35" autofocus/>
               <span class="input-group-btn">
               </span>
         </div>
